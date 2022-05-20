0f60bda5983b39c7d0ae1dcce65b85c37ce0365b
ajit51 <ajitk433@gmail.com>
2022-05-20 08:08:18 +0530

    swagger configuration with spring security with jwt

--- src/main/java/com/blog/config/SwaggerConfig.java
+++ src/main/java/com/blog/config/SwaggerConfig.java
@@ -1,6 +1,8 @@
 package com.blog.config;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,16 +10,23 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
 
+	private static final String AUTHORIZATION_HEADER = "Authorization";
+
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).apiInfo(getInfo()).select().apis(RequestHandlerSelectors.any())
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(getInfo()).securityContexts(securityContexts())
+				.securitySchemes(Arrays.asList(apiKey())).select().apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any()).build();
 	}
 
@@ -26,5 +35,18 @@ public class SwaggerConfig {
 				"This project is developed by Learn Code With Durgesh", "1.0", "Terms of service",
 				new Contact("Durgesh", "https://learncodewithdurgesh.com", "learncodewithdurgesh@gmail.com"),
 				"License of APIS", "API License URL", Collections.EMPTY_LIST);
+	}
+
+	private ApiKey apiKey() {
+		return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+	}
+
+	private List<SecurityContext> securityContexts() {
+		return Arrays.asList(SecurityContext.builder().securityReferences(securityReferences()).build());
+	}
+
+	private List<SecurityReference> securityReferences() {
+		AuthorizationScope scope = new AuthorizationScope("global", "accessEverything");
+		return Arrays.asList(new SecurityReference("JWT", new AuthorizationScope[] { scope }));
 	}
 }
